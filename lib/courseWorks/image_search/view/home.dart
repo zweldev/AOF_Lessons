9c05957d9ce259d1f81e81e4b107461710b1fe6c
zweldev <zweldev@gmail.com>
2022-12-26 10:33:07 +0630

    networkfetch_with_starlight_typeAhead_package

--- lib/courseWorks/image_search/view/home.dart
+++ lib/courseWorks/image_search/view/home.dart
@@ -1,5 +1,6 @@
 import 'package:aof_lessons/courseWorks/image_search/service/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:starlight_type_ahead/starlight_type_ahead.dart';
 
 class ImageSearchViewEXP extends StatefulWidget {
   const ImageSearchViewEXP({super.key});
@@ -22,29 +23,18 @@ class _ImageSearchViewEXPState extends State<ImageSearchViewEXP> {
         child: Center(
           child: Column(
             children: [
-              TextField(
-                onChanged: (value) {
-                  List<String>? _data = API_service.instance().available?.dogs;
-
-                  final List<String>? result = _data
-                      ?.where((element) => element
-                          .toLowerCase()
-                          .replaceAll(" ", "")
-                          .contains(value.toLowerCase().replaceAll(" ", "")))
-                      .toList();
-                  textToShow.clear();
-                  setState(() {});
-                  if (result?.isNotEmpty == true) {
-                    textToShow.clear();
-                    textToShow.addAll(result!);
-                    setState(() {});
-                  } else {
-                    textToShow.clear();
-                    setState(() {});
-                  }
-                },
-              ),
-              for (String x in textToShow) Text(x),
+              StarlightTypeAhead(
+                  controller: TextEditingController(),
+                  data: API_service.instance().available?.dogs,
+                  width: MediaQuery.of(context).size.width,
+                  height: 600,
+                  itemHeight: 50,
+                  itemBuilder: (e) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text("$e"),
+                    );
+                  })
             ],
           ),
         ),
